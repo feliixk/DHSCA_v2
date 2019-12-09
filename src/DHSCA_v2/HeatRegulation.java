e179f2646732be078044a76560b4a4ac0fb409b2
simonS95 <simon.sorensen@outlook.com>
2019-12-09 16:35:45 +0100

    fixade metoder

--- src/DHSCA_v2/HeatRegulation.java
+++ src/DHSCA_v2/HeatRegulation.java
@@ -1,4 +1,14 @@
 package DHSCA_v2;
 
-public class HeatRegulation {
+public class HeatRegulation extends SensorValue {
+    private double percentageValue;
+
+    public HeatRegulation(double percentageValue, String timeStamp){
+        super(timeStamp);
+        this.percentageValue = percentageValue;
+    }
+
+    public double readHeatValueFromKeyBoard(){
+        return 0.0; //returnerar 0.0 för att förhindra error
+    }
 }
--- src/DHSCA_v2/Temperature.java
+++ src/DHSCA_v2/Temperature.java
@@ -1,9 +1,9 @@
 package DHSCA_v2;
 
 public class Temperature extends SensorValue {
-    private float degrees;
+    private double degrees;
 
-    public Temperature(float degrees, String timeStamp) {
+    public Temperature(double degrees, String timeStamp) {
         super(timeStamp);
         this.degrees = degrees;
     }
