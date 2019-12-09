7630fa540c24033d3e1eb4bff2e6adf81311bc66
feliixk <felixkivioja95@gmail.com>
2019-12-09 18:00:29 +0100

    Added addApartmentOwner method

--- src/DHSCA_v2/Admin.java
+++ src/DHSCA_v2/Admin.java
@@ -1,12 +1,28 @@
 package DHSCA_v2;
 
+import java.util.Scanner;
+
 public class Admin extends User {
 
     public Admin(String user, String pass){
         super(user, pass);
     }
 
-    public void addApartmentOwner(){
+    public User addApartmentOwner(){
+        String user, pass;
+        int aptNumber;
+        int rentCost;
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the name of owner?");
+        user = input.nextLine();
+        System.out.println("What is the password of owner?");
+        pass = input.nextLine();
+        System.out.println("What is the apartment number");
+        aptNumber = Integer.parseInt(input.nextLine());
+        System.out.println("What is the rent cost?");
+        rentCost = Integer.parseInt(input.nextLine());
+
+        return new ApartmentOwner(user, pass, aptNumber, rentCost);
 
     }
 
