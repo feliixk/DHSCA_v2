a9ee66f34002b334bbf7616a0ce71136f76ff0ea
simonS95 <simon.sorensen@outlook.com>
2019-12-09 18:25:51 +0100

    fixat med login och lade till showmenu()

--- src/DHSCA_v2/DistrictHeatSavingsApp.java
+++ src/DHSCA_v2/DistrictHeatSavingsApp.java
@@ -17,6 +17,8 @@ public class DistrictHeatSavingsApp {
 
         DHSCA.currentLoggedInUser = DHSCA.login();
         System.out.println("<Inloggad som: "+DHSCA.currentLoggedInUser.toString()+">");
+
+        DHSCA.showMenu();
     }
 
     public User login(){
@@ -26,7 +28,6 @@ public class DistrictHeatSavingsApp {
         String username = input.nextLine();
         System.out.println("Please enter your password: ");
         String password = input.nextLine();
-        input.close();
 
         for (User userInLoop :
                 userArrayList) {
@@ -40,5 +41,68 @@ public class DistrictHeatSavingsApp {
         }
         //TODO[SS]: Gör att login inte kan krascha vid input
         return result;
+    }
+
+    public void showMenu() {
+        int choice;
+        Scanner input = new Scanner(System.in);
+
+        do {
+            if (currentLoggedInUser instanceof Admin) {
+                System.out.println("[1] Add apartment" +
+                        "\n[2] Add outdoor temperature measurement" +
+                        "\n[3] Show today's outdoor measurements" +
+                        "\n[4] Show last 7 days outdoor measurements" +
+                        "\n[5] Show average heat setting" +
+                        "\n[0] EXIT");
+            } else if (currentLoggedInUser instanceof ApartmentOwner) {
+                System.out.println("[1] Add indoor temperature" +
+                        "\n[2] Change heat value" +
+                        "\n[3] Show average heat setting" +
+                        "\n[4] Show today's indoor measurement" +
+                        "\n[5] Show last 7 day's indoor measurement" +
+                        "\n[0] EXIT");
+            }
+            choice = input.nextInt();
+            //TODO[SS]: Gör så den inte kan krascha vid input
+
+            switch (choice) {
+                case 1:
+                    if (currentLoggedInUser instanceof Admin) {
+                        System.out.println("<Du valde add apartment>");
+                    } else if (currentLoggedInUser instanceof ApartmentOwner) {
+                        System.out.println("<Du valde add indoor temperature>");
+                    }
+                    break;
+                case 2:
+                    if (currentLoggedInUser instanceof Admin) {
+                        System.out.println("<Du valde add outdoor temperature measurement>");
+                    } else if (currentLoggedInUser instanceof ApartmentOwner) {
+                        System.out.println("<Du valde change heat value>");
+                    }
+                    break;
+                case 3:
+                    if (currentLoggedInUser instanceof Admin) {
+                        System.out.println("<Du valde show today's outdoor measurements>");
+                    } else if (currentLoggedInUser instanceof ApartmentOwner) {
+                        System.out.println("<Du valde show average heat setting>");
+                    }
+                    break;
+                case 4:
+                    if (currentLoggedInUser instanceof Admin) {
+                        System.out.println("<Du valde show last 7 days outdoor measurements>");
+                    } else if (currentLoggedInUser instanceof ApartmentOwner) {
+                        System.out.println("<Du valde show today's indoor measurement>");
+                    }
+                    break;
+                case 5:
+                    if (currentLoggedInUser instanceof Admin) {
+                        System.out.println("<Du valde show average heat setting>");
+                    } else if (currentLoggedInUser instanceof ApartmentOwner) {
+                        System.out.println("<Du valde show last 7 day's indoor measurement>");
+                    }
+                    break;
+            }
+        } while (choice != 0);
     }
 }
