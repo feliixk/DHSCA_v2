a64e55dac8aadceff02122b1dd2f4ef43f31c7ed
feliixk <felixkivioja95@gmail.com>
2019-12-09 16:42:32 +0100

    Added constructor & methods

--- src/DHSCA_v2/Admin.java
+++ src/DHSCA_v2/Admin.java
@@ -1,4 +1,16 @@
 package DHSCA_v2;
 
-public class Admin {
+public class Admin extends User {
+
+    public Admin(String user, String pass){
+        super(user, pass);
+    }
+
+    public void addApartmentOwner(){
+
+    }
+
+    public void addOutdoorTemp(){
+
+    }
 }
