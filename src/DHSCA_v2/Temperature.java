a9716a154dfc5e178de1115b8d2a210fb3d1aafa
Ibrahim Ahmed Ali <ibbe2025@protonmail.com>
2019-12-10 12:37:43 +0100

    har lagt till method till temp

--- src/DHSCA_v2/Temperature.java
+++ src/DHSCA_v2/Temperature.java
@@ -1,10 +1,17 @@
 package DHSCA_v2;
 
+import java.util.Scanner;
+
 public class Temperature extends SensorValue {
     private double degrees;
 
     public Temperature(double degrees, String timeStamp) {
         super(timeStamp);
         this.degrees = degrees;
+    }
+    public double readTempFromKeyboard(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please input the current temperature.");
+        return input.nextDouble();
     }
 }
