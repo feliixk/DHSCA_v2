f18b664fbdaf3d3175f93c756fc293de9cdb714e
Valter Sjöstrand <Valter.sjo@gmail.com>
2019-12-10 13:14:08 +0100

    Fixed problem with Temp class

--- src/DHSCA_v2/Temperature.java
+++ src/DHSCA_v2/Temperature.java
@@ -3,11 +3,23 @@ package DHSCA_v2;
 import java.util.Scanner;
 
 public class Temperature extends SensorValue {
+    Scanner input = new Scanner(System.in);
     private double degrees;
 
     public Temperature(double degrees, String timeStamp) {
         super(timeStamp);
         this.degrees = degrees;
+    }
+
+
+    public double readTempFromKeyboard(){
+        System.out.println("Enter what temperature it is in you apartment!");
+        System.out.print("> ");
+        double degrees = input.nextDouble();
+        input.nextLine();
+
+        return degrees;
+
     }
 
 }
