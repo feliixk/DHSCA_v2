d1ab291e3e065a2890ee5b21cc34f0268d9f0e83
feliixk <felixkivioja95@gmail.com>
2019-12-10 20:37:21 +0100

    Added method to change heating value

--- src/DHSCA_v2/ApartmentOwner.java
+++ src/DHSCA_v2/ApartmentOwner.java
@@ -7,6 +7,7 @@ public class ApartmentOwner extends User {
     public int rentCost;
     public String buildingAddress;
     private IndoorTemp indoorTemp = new IndoorTemp(0,0.0,"");
+    private HeatRegulation heatRegulation = new HeatRegulation(0,0, "");
 
 
     public ApartmentOwner(String name, String pass, int apartmentNumber, int rentCost, String buildingAddress){
@@ -24,12 +25,13 @@ public class ApartmentOwner extends User {
 
     }
 
-   // @Override
-   // public String toString() {
-   //     return "ApartmentOwner{" +
-   //             "user='" + user + '\'' +
-   //             '}';
-   // }
+    public HeatRegulation changeHeatingValue(User user){
+        String timeStamp = heatRegulation.readTimestamp();
+        double heatValue = heatRegulation.readHeatValueFromKeyBoard();
+        int aptNumber = ((ApartmentOwner) user).getApartmentNumber();
+        return new HeatRegulation(aptNumber, heatValue, timeStamp);
+
+    }
 
     public int getApartmentNumber() {
         return apartmentNumber;
