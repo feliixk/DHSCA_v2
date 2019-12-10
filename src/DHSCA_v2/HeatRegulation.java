dd7743451ea7d5bea45e7c9be20f1795b8ff7a32
feliixk <felixkivioja95@gmail.com>
2019-12-10 20:38:30 +0100

    Added readHeatValueFromKeyboard method

--- src/DHSCA_v2/HeatRegulation.java
+++ src/DHSCA_v2/HeatRegulation.java
@@ -1,14 +1,29 @@
 package DHSCA_v2;
+import java.util.Scanner;
 
 public class HeatRegulation extends SensorValue {
     private double percentageValue;
+    public int aptNumber;
 
-    public HeatRegulation(double percentageValue, String timeStamp){
+
+    public HeatRegulation(int aptNumber, double percentageValue, String timeStamp){
         super(timeStamp);
         this.percentageValue = percentageValue;
+        this.aptNumber = aptNumber;
     }
 
     public double readHeatValueFromKeyBoard(){
-        return 0.0; //returnerar 0.0 för att förhindra error
+        double heatValue = 0;
+        boolean cont = true;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input desired heat value: ");
+        System.out.print("> ");
+
+        heatValue = Double.parseDouble(input.nextLine());
+        return heatValue;
+    }
+
+    public double getPercentageValue(){
+        return percentageValue;
     }
 }
