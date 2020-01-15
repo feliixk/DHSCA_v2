4ebbef75e5297140f22b2de509a668f4fba922a3
feliixk <felixkivioja95@gmail.com>
2020-01-15 20:34:23 +0100

    hotfix

--- src/DHSCA_v2/ApartmentOwner.java
+++ src/DHSCA_v2/ApartmentOwner.java
@@ -267,7 +267,7 @@ public class ApartmentOwner extends User {
 
             }
         }
-        
+
         final String regex = "[2][0][\\d]{2}[-]([0][\\d]|([1][0-2]))[-]([0][1-9]|[1-2][\\d]|[3][0-1])[ ]([0-1]{1}[\\d]{1}|([2][0-3]))[:][0-5][\\d][:][0-5][\\d]";
         String dateInput1 = "";
         String dateInput2 = "";
